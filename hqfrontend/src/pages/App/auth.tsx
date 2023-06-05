4319c4358a69cc262d81d8b1fee4446cf6c6830a
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-05 19:12:47 -0400

    Logins retry

--- hqfrontend/src/pages/App/auth.tsx
+++ hqfrontend/src/pages/App/auth.tsx
@@ -12,13 +12,22 @@ const REFRESH_TOKEN_MUTATION = gql`
 
 export const useAuth = (setLoggedIn: (value: boolean) => void, setGlobalUsername: (username: string) => void) => {
 
+	let retryCount = 0;
+	const MAX_RETRIES = 2;
+
 	const [refreshToken] = useMutation(REFRESH_TOKEN_MUTATION, {
 		onError: () => {
-			setLoggedIn(false);
-			localStorage.removeItem('loggedIn');
-			setGlobalUsername("");
+			if (retryCount < MAX_RETRIES) {
+				retryCount++;
+				refreshToken();
+			} else {
+				setLoggedIn(false);
+				localStorage.removeItem('loggedIn');
+				setGlobalUsername("");
+			}
 		},
 		onCompleted: (data) => {
+			retryCount = 0;  // reset the retry count after a successful request
 			setLoggedIn(true);
 			localStorage.setItem('loggedIn', "true");
 			setGlobalUsername(data.refreshToken.payload.username);

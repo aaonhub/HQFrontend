9fbc3033d7a48bff4d1169edcf1b562327e869c4
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-06 03:44:45 -0400

    Made the date display look better

--- hqfrontend/src/pages/TodayPage/1TodayPage.tsx
+++ hqfrontend/src/pages/TodayPage/1TodayPage.tsx
@@ -1,24 +1,36 @@
 import { Grid, Typography } from "@mui/material";
-
 import Itinerary from "./Itinerary";
 import LogList from "./LogList";
-// import Calendar from "./Calendar";
 
-
-
+const getOrdinalSuffix = (day: number): string => {
+	if (day > 3 && day < 21) return 'th';
+	switch (day % 10) {
+		case 1: return "st";
+		case 2: return "nd";
+		case 3: return "rd";
+		default: return "th";
+	}
+}
 
 const TodayPage = () => {
+	// Get the current date
+	const currentDate = new Date();
 
+	// Format each part of the date
+	const dayWithSuffix = `${currentDate.getDate()}${getOrdinalSuffix(currentDate.getDate())}`;
+	const month = currentDate.toLocaleString(undefined, { month: 'long' });
+	const year = currentDate.getFullYear();
+	const weekday = currentDate.toLocaleString(undefined, { weekday: 'long' });
 
+	// Combine parts into the final date string
+	const customFormattedDate = `${weekday}, ${month} ${dayWithSuffix}, ${year}`;
 
 	return (
 		<Grid container>
-
-
 			{/* Day Display */}
 			<Grid item xs={12}>
-				<Typography variant="h4">
-					{new Date().toLocaleDateString()}
+				<Typography variant="h4" sx={{ paddingBottom: 2 }}>
+					{customFormattedDate}
 				</Typography>
 			</Grid>
 
@@ -33,7 +45,6 @@ const TodayPage = () => {
 					<LogList />
 				</Grid>
 			</Grid>
-
 		</Grid>
 	);
 };

bde94ba393c4b0c473904cfbc4f6b6d91c394240
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-19 10:14:05 -0400

    Working calendar

--- hqfrontend/src/pages/TodayPage/Calendar.tsx
+++ hqfrontend/src/pages/TodayPage/Calendar.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin, { Draggable } from '@fullcalendar/interaction';
 import { Card, CardContent } from '@mui/material';
+import { EventInput } from '@fullcalendar/core';
 
 const Calendar = () => {
 	const now = new Date().toLocaleTimeString('en-US', {
@@ -10,6 +12,59 @@ const Calendar = () => {
 		minute: '2-digit',
 		second: '2-digit',
 	});
+
+	const [events, setEvents] = useState<EventInput[]>([
+		{
+			id: '1',
+			title: 'my event',
+			start: '2023-06-18T10:00:00',
+			end: '2023-06-18T12:00:00',
+		},
+	]);
+	const calendarRef = useRef(null);
+
+	useEffect(() => {
+		let draggableEl = document.getElementById('external-events');
+
+		if (draggableEl) {
+			new Draggable(draggableEl, {
+				itemSelector: '.event-class',
+				eventData: function (eventEl) {
+					let event = JSON.parse(eventEl.getAttribute('data-event') ?? '{}');
+					return {
+						title: event?.title,
+						duration: event?.duration,
+					};
+				},
+			});
+		}
+	}, []);
+
+	const handleEventChange = (changeInfo: any) => {
+		console.log(changeInfo.event.start);
+		// Find the index of the event that was changed
+		const index = events.findIndex(event => event.id === changeInfo.event.id);
+
+		// Create a new array of events
+		const newEvents = [...events];
+
+		// Update the start and end times of the changed event
+		newEvents[index].start = changeInfo.event.start;
+		newEvents[index].end = changeInfo.event.end;
+
+		// Update the state
+		setEvents(newEvents);
+	}
+
+	const eventReceive = (info: any) => {
+		// Handle the actual event creation
+		setEvents([...events, {
+			id: info.event.id,
+			title: info.event.title,
+			start: info.event.start,
+			end: info.event.end
+		}]);
+	};
 
 	return (
 		<Card sx={{
@@ -20,8 +75,23 @@ const Calendar = () => {
 			marginRight: 2,
 		}}>
 			<CardContent sx={{ height: '100%', padding: 1 }}>
+				<div id='external-events'>
+					{events.map((event) => (
+						<div
+							key={event.id}
+							className="event-class"
+							data-event={JSON.stringify({
+								title: event.title,
+								duration: '02:00'
+							})}
+						>
+							{event.title}
+						</div>
+					))}
+				</div>
 				<FullCalendar
-					plugins={[timeGridPlugin]}
+					ref={calendarRef}
+					plugins={[timeGridPlugin, interactionPlugin]}
 					initialView="timeGridDay"
 					weekends={true}
 					headerToolbar={false}
@@ -29,6 +99,11 @@ const Calendar = () => {
 					scrollTime={now}
 					height="100%"
 					contentHeight="100%"
+					droppable={true}
+					eventReceive={eventReceive}
+					events={events}
+					eventChange={handleEventChange}
+					editable={true}
 				/>
 			</CardContent>
 		</Card>

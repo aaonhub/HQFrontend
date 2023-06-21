30243f39600199e038bce83b9e01d59e8fd788d1
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-21 19:54:57 -0400

    Extended the day to 3am instead of midnight

--- hqfrontend/src/components/DateFunctions.ts
+++ hqfrontend/src/components/DateFunctions.ts
@@ -1,11 +1,45 @@
 
 export function getCurrentLocalDate() {
 	const now = new Date();
+	if (now.getHours() < 3) {
+		now.setDate(now.getDate() - 1);
+	}
+	console.log(now)
 	const year = now.getFullYear();
 	const month = String(now.getMonth() + 1).padStart(2, '0');
 	const day = String(now.getDate()).padStart(2, '0');
 
 	return `${year}-${month}-${day}`;
+}
+
+function getOrdinalSuffix(i: number) {
+	const j = i % 10;
+	const k = i % 100;
+	if (j == 1 && k != 11) {
+		return i + "st";
+	}
+	if (j == 2 && k != 12) {
+		return i + "nd";
+	}
+	if (j == 3 && k != 13) {
+		return i + "rd";
+	}
+	return i + "th";
+}
+export function getCustomLocalDate() {
+	let currentDate = new Date();
+	if (currentDate.getHours() < 3) {
+		currentDate.setDate(currentDate.getDate() - 1);
+	}
+
+	const dayWithSuffix = getOrdinalSuffix(currentDate.getDate());
+	const month = currentDate.toLocaleString(undefined, { month: 'long' });
+	const year = currentDate.getFullYear();
+	const weekday = currentDate.toLocaleString(undefined, { weekday: 'long' });
+
+	const customFormattedDate = `${weekday}, ${month} ${dayWithSuffix}, ${year}`;
+
+	return customFormattedDate;
 }
 
 export function convertDateToLocal(date: Date) {
@@ -29,6 +63,8 @@ export function getCurrentDayOfMonth() {
 	return now.getDate();
 }
 
+
+// Conversion functions
 export function dateToYYYYMMDD(date: Date | null) {
 	if (!date) {
 		return "";
--- hqfrontend/src/pages/TodayPage/1TodayPage.tsx
+++ hqfrontend/src/pages/TodayPage/1TodayPage.tsx
@@ -1,29 +1,11 @@
 import { Container, Grid, Typography } from "@mui/material";
 import Itinerary from "./Itinerary/Itinerary";
 import LogList from "./LogList";
+import { getCustomLocalDate } from "../../components/DateFunctions";
 
-const getOrdinalSuffix = (day: number): string => {
-	if (day > 3 && day < 21) return 'th';
-	switch (day % 10) {
-		case 1: return "st";
-		case 2: return "nd";
-		case 3: return "rd";
-		default: return "th";
-	}
-}
+
 
 const TodayPage = () => {
-	// Get the current date
-	const currentDate = new Date();
-
-	// Format each part of the date
-	const dayWithSuffix = `${currentDate.getDate()}${getOrdinalSuffix(currentDate.getDate())}`;
-	const month = currentDate.toLocaleString(undefined, { month: 'long' });
-	const year = currentDate.getFullYear();
-	const weekday = currentDate.toLocaleString(undefined, { weekday: 'long' });
-
-	// Combine parts into the final date string
-	const customFormattedDate = `${weekday}, ${month} ${dayWithSuffix}, ${year}`;
 
 	return (
 		<Container>
@@ -31,7 +13,7 @@ const TodayPage = () => {
 				{/* Day Display */}
 				<Grid item xs={12}>
 					<Typography variant="h4" sx={{ paddingBottom: 2 }}>
-						{customFormattedDate}
+						{getCustomLocalDate()}
 					</Typography>
 				</Grid>
 

466880d8e5ddae4640ed6fe3d72ca55175c0d861
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-24 18:49:38 -0400

    fixed itinerary

--- hqfrontend/src/components/DateFunctions.ts
+++ hqfrontend/src/components/DateFunctions.ts
@@ -4,7 +4,6 @@ export function getCurrentLocalDate() {
 	if (now.getHours() < 3) {
 		now.setDate(now.getDate() - 1);
 	}
-	console.log(now)
 	const year = now.getFullYear();
 	const month = String(now.getMonth() + 1).padStart(2, '0');
 	const day = String(now.getDate()).padStart(2, '0');

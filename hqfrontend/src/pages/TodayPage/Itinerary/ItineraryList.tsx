e1d471dea33fdcc38a6317ee017c7079101f5a40
aaon <54234552+aaonhub@users.noreply.github.com>
2023-06-30 21:28:09 -0400

    Now highlights items based on overdue status

--- hqfrontend/src/pages/TodayPage/Itinerary/ItineraryList.tsx
+++ hqfrontend/src/pages/TodayPage/Itinerary/ItineraryList.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 import { Checkbox, List, ListItem, ListItemText } from '@mui/material'
 import { Draggable } from '@fullcalendar/interaction'
 import { v4 as uuidv4 } from 'uuid';
+import { addLengthToTime, currentLocalTime } from '../../../components/DateFunctions';
 
 interface ItineraryListProps {
     list: any
@@ -12,6 +13,8 @@ interface ItineraryListProps {
 
 const ItineraryList: React.FC<ItineraryListProps> = ({ list, setSelectedInboxItemId, setSelectedHabitId, handleCheckItem }) => {
     const [id] = useState(uuidv4());
+
+
 
     // Calendar Stuff
     useEffect(() => {
@@ -36,45 +39,68 @@ const ItineraryList: React.FC<ItineraryListProps> = ({ list, setSelectedInboxIte
             sx={{ padding: 0 }}
             id={id}
         >
-            {list.map((item: any) => (
-                <ListItem
-                    key={item.id}
-                    disablePadding
-                    className="event-class"
-                    data-event={JSON.stringify({
-                        id: item.id,
-                        title: item.title,
-                        duration: item.length,
-                    })}
-                    
-                    onClick={() => {
-                        if ("i" === item.id.slice(-1)) {
-                            setSelectedInboxItemId(item.id.slice(0, -1))
+            {list.map((item: any) => {
+
+
+                let color = "grey"
+
+                if (item.startTime < currentLocalTime() && !item.completed) {
+                    if (!item.length) {
+                        color = "red"
+                    } else {
+                        if (addLengthToTime(item.startTime, item.length) > currentLocalTime()) {
+                            color = "blue"
+                        } else {
+                            color = "red"
                         }
-                        if ("h" === item.id.slice(-1)) {
-                            setSelectedHabitId(item.id.slice(0, -1))
-                        }
-                    }}
-                    sx={{
-                        borderRadius: 2, border: '1px solid grey', marginBottom: 1, cursor: 'pointer',
-                        '&:hover': {
-                            backgroundColor: 'black',
-                        },
-                    }}
-                >
-                    <Checkbox
-                        checked={item.completedToday}
-                        onClick={(event) => {
-                            event.stopPropagation()
-                            handleCheckItem(item)
+                    }
+                }
+
+                let itemColor;
+                if (color === "red") itemColor = '1px solid red';
+                else if (color === "blue") itemColor = '1px solid cyan';
+                else itemColor = '1px solid grey';
+
+                return (
+                    <ListItem
+                        key={item.id}
+                        disablePadding
+                        className="event-class"
+                        data-event={JSON.stringify({
+                            id: item.id,
+                            title: item.title,
+                            duration: item.length,
+                        })}
+
+                        onClick={() => {
+                            if ("i" === item.id.slice(-1)) {
+                                setSelectedInboxItemId(item.id.slice(0, -1))
+                            }
+                            if ("h" === item.id.slice(-1)) {
+                                setSelectedHabitId(item.id.slice(0, -1))
+                            }
                         }}
-                    />
-                    <ListItemText
-                        primary={item.title}
-                        secondary={item.startTime?.slice(0, -3)}
-                    />
-                </ListItem>
-            ))}
+                        sx={{
+                            borderRadius: 2, border: itemColor, marginBottom: 1, cursor: 'pointer',
+                            '&:hover': {
+                                backgroundColor: 'black',
+                            },
+                        }}
+                    >
+                        <Checkbox
+                            checked={item.completedToday}
+                            onClick={(event) => {
+                                event.stopPropagation()
+                                handleCheckItem(item)
+                            }}
+                        />
+                        <ListItemText
+                            primary={item.title}
+                            secondary={item.startTime?.slice(0, -3)}
+                        />
+                    </ListItem>
+                )
+            })}
         </List>
     )
 }

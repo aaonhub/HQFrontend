7923c3ee8e50f91fca4e884717dc75ba4f417e38
aaon <54234552+aaonhub@users.noreply.github.com>
2023-11-20 17:35:17 -0500

    Revert "Theme stuff"
    This reverts commit 3d53b2914aa0d9b629441282ed7f1cab9d41a355.

--- hqfrontend/src/pages/TodayPage/Components/ItineraryList.tsx
+++ hqfrontend/src/pages/TodayPage/Components/ItineraryList.tsx
@@ -1,75 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { Checkbox, List, ListItem, ListItemText, Typography, useTheme } from '@mui/material';
-import { Draggable } from '@fullcalendar/interaction';
+import React, { useEffect, useState } from 'react'
+import { Checkbox, List, ListItem, ListItemText, Typography } from '@mui/material'
+import { Draggable } from '@fullcalendar/interaction'
 import { v4 as uuidv4 } from 'uuid';
 import { addLengthToTime, currentLocalTime, getCurrentLocalDate, getCurrentLocalDateUnadjusted } from '../../../components/DateFunctions';
 import DescriptionIcon from '@mui/icons-material/Description';
-import { ReactSortable, SortableEvent } from 'react-sortablejs';
+import { ReactSortable, SortableEvent } from 'react-sortablejs'
 import EventIcon from '@mui/icons-material/Event';
 import { motion, AnimatePresence } from "framer-motion";
+
 
 // Models
 import SimpleItem from '../../../models/simpleitem';
 
+
 interface ItineraryListProps {
-	list: SimpleItem[];
-	setList: any;
-	setSelectedInboxItemId: any;
-	setSelectedHabitId: any;
-	setSelectedRitualId: any;
-	setSelectedEntryID: any;
-	handleCheckItem: any;
+	list: SimpleItem[]
+	setList: any
+	setSelectedInboxItemId: any
+	setSelectedHabitId: any
+	setSelectedRitualId: any
+	setSelectedEntryID: any
+	handleCheckItem: any
 }
 
-const ItineraryList: React.FC<ItineraryListProps> = ({
-	list,
-	setList,
-	setSelectedInboxItemId,
-	setSelectedHabitId,
-	setSelectedRitualId,
-	setSelectedEntryID,
-	handleCheckItem
-}) => {
-	const theme = useTheme();
+const ItineraryList: React.FC<ItineraryListProps> = ({ list, setList, setSelectedInboxItemId, setSelectedHabitId, setSelectedRitualId, setSelectedEntryID, handleCheckItem }) => {
 	const [id] = useState(uuidv4());
 
+
+
+	// Calendar Stuff
 	useEffect(() => {
-		let draggableEl = document.getElementById(id);
+		let draggableEl = document.getElementById(id)
 
 		if (draggableEl) {
 			new Draggable(draggableEl, {
 				itemSelector: '.event-class',
 				eventData: function (eventEl) {
-					let event = JSON.parse(eventEl.getAttribute('data-event') ?? '{}');
+					let event = JSON.parse(eventEl.getAttribute('data-event') ?? '{}')
 					return {
 						title: event?.id + '|' + event?.title,
 						duration: event?.duration,
-					};
+					}
 				}
-			});
+			})
 		}
-	}, [id]);
+	}, [id])
 
+
+
+
+
+	// Sortable Stuff
 	const handleOrderChange = (evt: SortableEvent) => {
-		const newIndex = evt.newIndex;
-		const oldIndex = evt.oldIndex;
+		const newIndex = evt.newIndex
+		const oldIndex = evt.oldIndex
 
 		if (typeof newIndex === 'undefined' || typeof oldIndex === 'undefined') {
-			return;
+			return
 		}
 
-		const newOrder = [...list];
-		newOrder.splice(newIndex, 0, newOrder.splice(oldIndex, 1)[0]);
+		const newOrder = [...list]
+		newOrder.splice(newIndex, 0, newOrder.splice(oldIndex, 1)[0])
 
-		setList(newOrder);
-	};
+		setList(newOrder)
+	}
 
 	if (list.length === 0) {
 		return (
 			<Typography variant="h6" align="center" color="textSecondary">
 				No items
 			</Typography>
-		);
+		)
 	}
 
 	return (
@@ -84,15 +85,38 @@ const ItineraryList: React.FC<ItineraryListProps> = ({
 				onEnd={handleOrderChange}
 			>
 				<AnimatePresence>
-					{list.map((item: SimpleItem) => {
-						let borderColor;
-						if (item.startDate && item.startDate < getCurrentLocalDateUnadjusted()) {
-							borderColor = theme.palette.error.main;
-						} else if (item.completedToday) {
-							borderColor = theme.palette.grey[500];
-						} else {
-							borderColor = theme.palette.primary.main;
+					{list.map((item: any) => {
+
+
+						// Border color
+						let color = "grey"
+						if (!item.startDate) {
+							item.startDate = getCurrentLocalDate()
 						}
+
+						if (item.startDate < getCurrentLocalDateUnadjusted()) {
+							color = "red"
+						} else if (!item.startTime) {
+							color = "grey"
+						} else if (item.completedToday) {
+							color = "grey"
+						} else
+							if (item.startTime < currentLocalTime()) {
+								if (!item.length) {
+									color = "red"
+								} else {
+									if (addLengthToTime(item.startTime, item.length) > currentLocalTime()) {
+										color = "blue"
+									} else {
+										color = "red"
+									}
+								}
+							}
+
+						let itemColor;
+						if (color === "red") itemColor = '1px solid red';
+						else if (color === "blue") itemColor = '1px solid cyan';
+						else itemColor = '1px solid grey';
 
 						return (
 							<motion.div
@@ -104,40 +128,52 @@ const ItineraryList: React.FC<ItineraryListProps> = ({
 									y: {
 										type: "spring",
 										stiffness: 50,
-										damping: 20,
-										mass: 1,
+										damping: 20,   // Added damping
+										mass: 1,       // Added mass
 									},
 									default: { duration: .5 },
 								}}
 								style={{ overflow: "hidden" }}
 							>
 								<ListItem
+									key={item.id}
 									disablePadding
 									onClick={() => {
-										if (item.type === "inbox") {
-											setSelectedInboxItemId(item.id);
-										} else if (item.type === "habit") {
-											setSelectedHabitId(item.id);
-										} else if (item.type === "ritual") {
-											setSelectedRitualId(item.id);
-											setSelectedEntryID(item.id);
+										if ("i" === item.id.slice(-1)) {
+											setSelectedInboxItemId(item.id.slice(0, -1))
+										}
+										if ("h" === item.id.slice(-1)) {
+											setSelectedHabitId(item.id.slice(0, -1))
+										}
+										if (item.type === "ritual") {
+											setSelectedRitualId(item.ritualID)
+											setSelectedEntryID(item.id.slice(0, -1))
 										}
 									}}
 									sx={{
 										borderRadius: 2,
-										border: `1px solid ${borderColor}`,
+										border: itemColor,
 										marginBottom: 1,
 										cursor: 'pointer',
 										'&:hover': {
-											backgroundColor: theme.palette.action.hover,
+											backgroundColor: 'black',
 										},
 									}}
 								>
+									<div
+										style={{
+											width: 5,
+											height: '100%',
+											backgroundColor: "black",
+											marginRight: 1,
+										}}
+									/>
+
 									<Checkbox
 										checked={item.completedToday}
 										onClick={(event) => {
-											event.stopPropagation();
-											handleCheckItem(item);
+											event.stopPropagation()
+											handleCheckItem(item)
 										}}
 										sx={{
 											marginRight: 1,
@@ -167,20 +203,24 @@ const ItineraryList: React.FC<ItineraryListProps> = ({
 											marginRight: 2,
 											marginLeft: 1,
 											cursor: 'grab',
-											color: item.type === 'ritual' ? theme.palette.error.main :
-												item.type === 'habit' ? theme.palette.success.main :
-													theme.palette.info.main
+											// backgroundColor: "red",
+											height: "100%",
+											// if ritual then red, if habit then blue, if inbox then grey
+											color: item.type === 'ritual' ? '#ffcccb' :
+												item.type === 'habit' ? '#90EE90' :
+													item.type === 'inbox' ? '#ADD8E6' :
+														''
 										}}
 									/>
 
 								</ListItem>
 							</motion.div>
-						);
+						)
 					})}
 				</AnimatePresence>
 			</ReactSortable>
 		</List>
-	);
+	)
 }
 
-export default ItineraryList;
+export default ItineraryList

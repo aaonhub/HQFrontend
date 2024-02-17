1a9c781bd12fb40d0d7fedb2ab60bd544e3818cd
aaon <54234552+aaonhub@users.noreply.github.com>
2024-02-17 04:23:15 -0500

    Add "type" field to package.json and comment out unused import

--- hqfrontend/src/index.tsx
+++ hqfrontend/src/index.tsx
@@ -5,7 +5,7 @@ import App from './pages/App/App';
 import { ApolloClient, InMemoryCache, from, ApolloProvider, HttpLink } from "@apollo/client";
 import { onError } from "@apollo/client/link/error";
 import { GlobalContextProvider } from './pages/App/GlobalContextProvider';
-import { fetch } from 'cross-fetch';
+// import { fetch } from 'cross-fetch';
 
 const errorLink = onError(({ graphQLErrors, networkError }) => {
 	if (graphQLErrors)
@@ -23,39 +23,39 @@ const graphqlURI = process.env.NODE_ENV === 'production'
 
 // const graphqlURI = "https://hqlink.herokuapp.com/graphql/"
 
-const customFetch = (uri: RequestInfo, options?: RequestInit): Promise<Response> => {
-	const retries = 10;
-	const retryDelay = 1000; // milliseconds
+// const customFetch = (uri: RequestInfo, options?: RequestInit): Promise<Response> => {
+// 	const retries = 10;
+// 	const retryDelay = 1000; // milliseconds
 
-	let attempts = 0;
-	const fetchWithRetry = (uri: RequestInfo, options?: RequestInit): Promise<Response> => {
-		return fetch(uri, options)
-			.then(response => {
-				if (response.status !== 200) {
-					return response.json().then((body) => {
-						if (body.errors && body.errors.some((error: any) => error.message === "You do not have permission to perform this action")) {
-							attempts += 1;
-							if (attempts <= retries) {
-								return new Promise(resolve =>
-									setTimeout(() => resolve(fetchWithRetry(uri, options)), retryDelay)
-								) as Promise<Response>
-							}
-						}
-						throw new Error(body.errors)
-					})
-				}
-				return response
-			})
-	}
+// 	let attempts = 0;
+// 	const fetchWithRetry = (uri: RequestInfo, options?: RequestInit): Promise<Response> => {
+// 		return fetch(uri, options)
+// 			.then(response => {
+// 				if (response.status !== 200) {
+// 					return response.json().then((body) => {
+// 						if (body.errors && body.errors.some((error: any) => error.message === "You do not have permission to perform this action")) {
+// 							attempts += 1;
+// 							if (attempts <= retries) {
+// 								return new Promise(resolve =>
+// 									setTimeout(() => resolve(fetchWithRetry(uri, options)), retryDelay)
+// 								) as Promise<Response>
+// 							}
+// 						}
+// 						throw new Error(body.errors)
+// 					})
+// 				}
+// 				return response
+// 			})
+// 	}
 
-	return fetchWithRetry(uri, options);
-};
+// 	return fetchWithRetry(uri, options);
+// };
 
 
 const httpLink = new HttpLink({
 	uri: graphqlURI,
 	credentials: 'include',
-	fetch: customFetch as any
+	// fetch: customFetch as any
 });
 
 
